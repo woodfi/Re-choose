da924293b9893dcda3951bb8bbc0feee941225ba
KT <26391179+VutonDesign@users.noreply.github.com>
2017-10-08 12:26:33 +0100

    Create index.py

---
+++ index.py
@@ -0,0 +1,49 @@
+# Import the modules
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = raw_input("Ask Re-choose a question")
+    
+    answers = random.randint(1,12)
+    
+    if question == "":
+        sys.exit()
+    
+    elif answers == 1:
+        print "For sure"
+    
+    elif answers == 2:
+        print "Exactly"
+    
+    elif answers == 3:
+        print "You may rely on it"
+    
+    elif answers == 4:
+        print "Possibly"
+    
+    elif answers == 5:
+        print "Concentrate and ask again"
+    
+    elif answers == 6:
+        print "Probably Not"
+    
+    elif answers == 7:
+        print "My reply is no"
+    
+    elif answers == 8:
+        print "My sources say no"
+        
+    elif answers == 9:
+        print "My sources say yes"
+        
+    elif answers == 10:
+        print "You probably will"
+        
+    elif answers == 11:
+        print "My sources say it will"
+        
+    elif answers == 12:
+        print "My sources say they could"
